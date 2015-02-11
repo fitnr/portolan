64580d127915f70e7260f1ca48dda6f970d094e8
fitnr <fitnr@fakeisthenewreal>
2015-02-11 16:20:52 -0500

    split off data

---
+++ portolan/data.py
@@ -0,0 +1,133 @@
+ABBRS = {
+    'nne': {
+        'trad': 'Greco-Tramontana', 'point': 'North-northeast', 'mid': 22.5, 'abbr': 'NNE'
+    },
+    'swbw': {
+        'trad': 'Quarto di Libeccio verso Ponente', 'point': 'Southwest by west', 'mid': 236.25, 'abbr': 'SWbW'
+    },
+    'nwbw': {
+        'trad': 'Quarto di Maestro verso Ponente', 'point': 'Northwest by west', 'mid': 303.75, 'abbr': 'NWbW'
+    },
+    'swbs': {
+        'trad': 'Quarto di Libeccio verso Ostro', 'point': 'Southwest by south', 'mid': 213.75, 'abbr': 'SWbS'
+    },
+    'nwbn': {
+        'trad': 'Quarto di Maestro verso Tramontana', 'point': 'Northwest by north', 'mid': 326.25, 'abbr': 'NWbN'
+    },
+    'ebs': {
+        'trad': 'Quarto di Levante verso Scirocco', 'point': 'East by south', 'mid': 101.25, 'abbr': 'EbS'
+    },
+    'nnw': {
+        'trad': 'Maestro-Tramontana', 'point': 'North-northwest', 'mid': 337.5, 'abbr': 'NNW'
+    },
+    'nbe': {
+        'trad': 'Quarto di Tramontana verso Greco', 'point': 'North by east', 'mid': 11.25, 'abbr': 'NbE'
+    },
+    'sebe': {
+        'trad': 'Quarto di Scirocco verso Levante', 'point': 'Southeast by east', 'mid': 123.75, 'abbr': 'SEbE'
+    },
+    'ene': {
+        'trad': 'Greco-Levante', 'point': 'East-northeast', 'mid': 67.5, 'abbr': 'ENE'
+    },
+    'nebn': {
+        'trad': 'Quarto di Greco verso Tramontana', 'point': 'Northeast by north', 'mid': 33.75, 'abbr': 'NEbN'
+    },
+    'nebe': {
+        'trad': 'Quarto di Greco verso Levante', 'point': 'Northeast by east', 'mid': 56.25, 'abbr': 'NEbE'
+    },
+    'ne': {
+        'trad': 'Greco', 'point': 'Northeast', 'mid': 45.0, 'abbr': 'NE'
+    },
+    'ese': {
+        'trad': 'Levante-Scirocco', 'point': 'East-southeast', 'mid': 112.5, 'abbr': 'ESE'
+    },
+    'sebs': {
+        'trad': 'Quarto di Scirocco verso Ostro', 'point': 'Southeast by south', 'mid': 146.25, 'abbr': 'SEbS'
+    },
+    'ebn': {
+        'trad': 'Quarto di Levante verso Greco', 'point': 'East by north', 'mid': 78.75, 'abbr': 'EbN'
+    },
+    'nw': {
+        'trad': 'Maestro', 'point': 'Northwest', 'mid': 315.0, 'abbr': 'NW'
+    },
+    'sbw': {
+        'trad': 'Quarto di Ostro verso Libeccio', 'point': 'South by west', 'mid': 191.25, 'abbr': 'SbW'
+    },
+    'nbw': {
+        'trad': 'Quarto di Tramontana verso Maestro', 'point': 'North by west', 'mid': 348.75, 'abbr': 'NbW'
+    },
+    'ssw': {
+        'trad': 'Ostro-Libeccio', 'point': 'South-southwest', 'mid': 202.5, 'abbr': 'SSW'
+    },
+    'wnw': {
+        'trad': 'Maestro-Ponente', 'point': 'West-northwest', 'mid': 292.5, 'abbr': 'WNW'
+    },
+    'sbe': {
+        'trad': 'Quarto di Ostro verso Scirocco', 'point': 'South by east', 'mid': 168.75, 'abbr': 'SbE'
+    },
+    'sse': {
+        'trad': 'Ostro-Scirocco', 'point': 'South-southeast', 'mid': 157.5, 'abbr': 'SSE'
+    },
+    'e': {
+        'trad': 'Levante', 'point': 'East', 'mid': 90.0, 'abbr': 'E'
+    },
+    'wbs': {
+        'trad': 'Quarto di Ponente verso Libeccio', 'point': 'West by south', 'mid': 258.75, 'abbr': 'WbS'
+    },
+    'sw': {
+        'trad': 'Libeccio', 'point': 'Southwest', 'mid': 225.0, 'abbr': 'SW'
+    },
+    'n': {
+        'trad': 'Tramontana', 'point': 'North', 'mid': 0.0, 'abbr': 'N'
+    },
+    'w': {
+        'trad': 'Ponente', 'point': 'West', 'mid': 270.0, 'abbr': 'W'
+    },
+    's': {
+        'trad': 'Ostro', 'point': 'South', 'mid': 180.0, 'abbr': 'S'
+    },
+    'wsw': {
+        'trad': 'Ponente-Libeccio', 'point': 'West-southwest', 'mid': 247.5, 'abbr': 'WSW'
+    },
+    'wbn': {
+        'trad': 'Quarto di Ponente verso Maestro', 'point': 'West by north', 'mid': 281.25, 'abbr': 'WbN'
+    },
+    'se': {
+        'trad': 'Scirocco', 'point': 'Southeast', 'mid': 135.0, 'abbr': 'SE'
+    }
+}
+
+SEGMENTS = {
+    0: ABBRS['n'],
+    1: ABBRS['nbe'],
+    2: ABBRS['nne'],
+    3: ABBRS['nebn'],
+    4: ABBRS['ne'],
+    5: ABBRS['nebe'],
+    6: ABBRS['ene'],
+    7: ABBRS['ebn'],
+    8: ABBRS['e'],
+    9: ABBRS['ebs'],
+    10: ABBRS['ese'],
+    11: ABBRS['sebe'],
+    12: ABBRS['se'],
+    13: ABBRS['sebs'],
+    14: ABBRS['sse'],
+    15: ABBRS['sbe'],
+    16: ABBRS['s'],
+    17: ABBRS['sbw'],
+    18: ABBRS['ssw'],
+    19: ABBRS['swbs'],
+    20: ABBRS['sw'],
+    21: ABBRS['swbw'],
+    22: ABBRS['wsw'],
+    23: ABBRS['wbs'],
+    24: ABBRS['w'],
+    25: ABBRS['wbn'],
+    26: ABBRS['wnw'],
+    27: ABBRS['nwbw'],
+    28: ABBRS['nw'],
+    29: ABBRS['nwbn'],
+    30: ABBRS['nnw'],
+    31: ABBRS['nbw']
+}
--- portolan/portolan.py
+++ portolan/portolan.py
@@ -1,263 +1,4 @@
-DATA = [
-    {
-        'abbreviation': 'N',
-        'max': 5.625,
-        'trad': 'Tramontana',
-        'point': 'North',
-        'mid': 0.0,
-        'min': 354.375
-    },
-    {
-        'abbreviation': 'NbE',
-        'max': 16.875,
-        'trad': 'Quarto di Tramontana verso Greco',
-        'point': 'North by east',
-        'mid': 11.25,
-        'min': 5.625
-    },
-    {
-        'abbreviation': 'NNE',
-        'max': 28.125,
-        'trad': 'Greco-Tramontana',
-        'point': 'North-northeast',
-        'mid': 22.5,
-        'min': 16.875
-    },
-    {
-        'abbreviation': 'NEbN',
-        'max': 39.375,
-        'trad': 'Quarto di Greco verso Tramontana',
-        'point': 'Northeast by north',
-        'mid': 33.75,
-        'min': 28.125
-    },
-    {
-        'abbreviation': 'NE',
-        'max': 50.625,
-        'trad': 'Greco',
-        'point': 'Northeast',
-        'mid': 45.0,
-        'min': 39.375
-    },
-    {
-        'abbreviation': 'NEbE',
-        'max': 61.875,
-        'trad': 'Quarto di Greco verso Levante',
-        'point': 'Northeast by east',
-        'mid': 56.25,
-        'min': 50.625
-    },
-    {
-        'abbreviation': 'ENE',
-        'max': 73.125,
-        'trad': 'Greco-Levante',
-        'point': 'East-northeast',
-        'mid': 67.5,
-        'min': 61.875
-    },
-    {
-        'abbreviation': 'EbN',
-        'max': 84.375,
-        'trad': 'Quarto di Levante verso Greco',
-        'point': 'East by north',
-        'mid': 78.75,
-        'min': 73.125
-    },
-    {
-        'abbreviation': 'E',
-        'max': 95.625,
-        'trad': 'Levante',
-        'point': 'East',
-        'mid': 90.0,
-        'min': 84.375
-    },
-    {
-        'abbreviation': 'EbS',
-        'max': 106.875,
-        'trad': 'Quarto di Levante verso Scirocco',
-        'point': 'East by south',
-        'mid': 101.25,
-        'min': 95.625
-    },
-    {
-        'abbreviation': 'ESE',
-        'max': 118.125,
-        'trad': 'Levante-Scirocco',
-        'point': 'East-southeast',
-        'mid': 112.5,
-        'min': 106.875
-    },
-    {
-        'abbreviation': 'SEbE',
-        'max': 129.375,
-        'trad': 'Quarto di Scirocco verso Levante',
-        'point': 'Southeast by east',
-        'mid': 123.75,
-        'min': 118.125
-    },
-    {
-        'abbreviation': 'SE',
-        'max': 140.625,
-        'trad': 'Scirocco',
-        'point': 'Southeast',
-        'mid': 135.0,
-        'min': 129.375
-    },
-    {
-        'abbreviation': 'SEbS',
-        'max': 151.875,
-        'trad': 'Quarto di Scirocco verso Ostro',
-        'point': 'Southeast by south',
-        'mid': 146.25,
-        'min': 140.625
-    },
-    {
-        'abbreviation': 'SSE',
-        'max': 163.125,
-        'trad': 'Ostro-Scirocco',
-        'point': 'South-southeast',
-        'mid': 157.5,
-        'min': 151.875
-    },
-    {
-        'abbreviation': 'SbE',
-        'max': 174.375,
-        'trad': 'Quarto di Ostro verso Scirocco',
-        'point': 'South by east',
-        'mid': 168.75,
-        'min': 163.125
-    },
-    {
-        'abbreviation': 'S',
-        'max': 185.625,
-        'trad': 'Ostro',
-        'point': 'South',
-        'mid': 180.0,
-        'min': 174.375
-    },
-    {
-        'abbreviation': 'SbW',
-        'max': 196.875,
-        'trad': 'Quarto di Ostro verso Libeccio',
-        'point': 'South by west',
-        'mid': 191.25,
-        'min': 185.625
-    },
-    {
-        'abbreviation': 'SSW',
-        'max': 208.125,
-        'trad': 'Ostro-Libeccio',
-        'point': 'South-southwest',
-        'mid': 202.5,
-        'min': 196.875
-    },
-    {
-        'abbreviation': 'SWbS',
-        'max': 219.375,
-        'trad': 'Quarto di Libeccio verso Ostro',
-        'point': 'Southwest by south',
-        'mid': 213.75,
-        'min': 208.125
-    },
-    {
-        'abbreviation': 'SW',
-        'max': 230.625,
-        'trad': 'Libeccio',
-        'point': 'Southwest',
-        'mid': 225.0,
-        'min': 219.375
-    },
-    {
-        'abbreviation': 'SWbW',
-        'max': 241.875,
-        'trad': 'Quarto di Libeccio verso Ponente',
-        'point': 'Southwest by west',
-        'mid': 236.25,
-        'min': 230.625
-    },
-    {
-        'abbreviation': 'WSW',
-        'max': 253.125,
-        'trad': 'Ponente-Libeccio',
-        'point': 'West-southwest',
-        'mid': 247.5,
-        'min': 241.875
-    },
-    {
-        'abbreviation': 'WbS',
-        'max': 264.375,
-        'trad': 'Quarto di Ponente verso Libeccio',
-        'point': 'West by south',
-        'mid': 258.75,
-        'min': 253.125
-    },
-    {
-        'abbreviation': 'W',
-        'max': 275.625,
-        'trad': 'Ponente',
-        'point': 'West',
-        'mid': 270.0,
-        'min': 264.375
-    },
-    {
-        'abbreviation': 'WbN',
-        'max': 286.875,
-        'trad': 'Quarto di Ponente verso Maestro',
-        'point': 'West by north',
-        'mid': 281.25,
-        'min': 275.625
-    },
-    {
-        'abbreviation': 'WNW',
-        'max': 298.125,
-        'trad': 'Maestro-Ponente',
-        'point': 'West-northwest',
-        'mid': 292.5,
-        'min': 286.875
-    },
-    {
-        'abbreviation': 'NWbW',
-        'max': 309.375,
-        'trad': 'Quarto di Maestro verso Ponente',
-        'point': 'Northwest by west',
-        'mid': 303.75,
-        'min': 298.125
-    },
-    {
-        'abbreviation': 'NW',
-        'max': 320.625,
-        'trad': 'Maestro',
-        'point': 'Northwest',
-        'mid': 315.0,
-        'min': 309.375
-    },
-    {
-        'abbreviation': 'NWbN',
-        'max': 331.875,
-        'trad': 'Quarto di Maestro verso Tramontana',
-        'point': 'Northwest by north',
-        'mid': 326.25,
-        'min': 320.625
-    },
-    {
-        'abbreviation': 'NNW',
-        'max': 343.125,
-        'trad': 'Maestro-Tramontana',
-        'point': 'North-northwest',
-        'mid': 337.5,
-        'min': 331.875
-    },
-    {
-        'abbreviation': 'NbW',
-        'max': 354.375,
-        'trad': 'Quarto di Tramontana verso Maestro',
-        'point': 'North by west',
-        'mid': 348.75,
-        'min': 343.125
-    }
-]
-
-ABBRS = None
+from .data import ABBRS, SEGMENTS
 
 
 def _by_(key, degree=None, abbr=None):
@@ -272,19 +13,14 @@ def _by_(key, degree=None, abbr=None):
 
 
 def _by_abbr(key, abbr):
-    global ABBRS
-    if not ABBRS:
-        ABBRS = {x['abbreviation'].lower(): x for x in DATA}
-
     return ABBRS[abbr.lower()][key]
 
 
 def _by_degree(key, degree):
-    degree = degree % 360
-
-    for item in DATA:
-        if (degree - item['min']) % 360 <= 11.24:
-            return item[key]
+    # 11.25 is 1/32 of a circle.
+    # rotate by half that (5.625) because the 0th segment straddles the origin
+    x = int(((degree + 5.625) % 360) / 11.25)
+    return SEGMENTS[x][key]
 
 
 def point(degree=None, abbr=None):
@@ -300,7 +36,8 @@ def abbr(degree):
 
 
 def degrees(abbr):
-    return _by_abbr('min', abbr), _by_abbr('mid', abbr), _by_abbr('max', abbr)
+    mid = middle(abbr)
+    return (mid - 5.625) % 360, mid, mid + 5.625
 
 
 def middle(abbr):
@@ -308,4 +45,5 @@ def middle(abbr):
 
 
 def range(abbr):
-    return (_by_abbr('min', abbr), _by_abbr('max', abbr))
+    a, _, b = degrees(abbr)
+    return (a, b)
